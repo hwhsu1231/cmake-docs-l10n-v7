672aa24ce7202f48951cdf45e86d97a5b291b4c4
Haowei Hsu <hwhsu1231@gmail.com>
2024-12-17 17:59:01 +0800

    test

--- cmake/mod1.cmake
+++
@@ -1,7 +0,0 @@
-
-message(STATUS "'mod1.cmake' is called.")
-
-include_guard()
-
-include(mod2)
-message(STATUS "'mod2.cmake' is included.")
--- cmake/mod2.cmake
+++
@@ -1,7 +0,0 @@
-
-message(STATUS "'mod2.cmake' is called.")
-
-include_guard()
-
-include(mod1)
-message(STATUS "'mod1.cmake' is included.")
