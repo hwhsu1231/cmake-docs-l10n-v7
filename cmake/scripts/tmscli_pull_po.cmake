1694c847f1d03bf4768afde2226258513b0b3e29
Haowei Hsu <hwhsu1231@gmail.com>
2025-01-01 01:24:57 +0800

    test

--- cmake/scripts/tmscli_pull_po.cmake
+++ cmake/scripts/tmscli_pull_po.cmake
@@ -29,7 +29,7 @@ foreach(_LANGUAGE ${LANGUAGE_LIST})
 
     message(STATUS "Preparing to pull '${_LANGUAGE_CROWDIN}' translations for '${VERSION}' version from TMS...")
     set(TMSTMP_PO_DIR   "${PROJ_L10N_VERSION_TMSTMP_DIR}/${_LANGUAGE}")
-    set(TMSTMP_PO_FILE  "${PROJ_L10N_VERSION_TMSTMP_DIR}/${_LANGUAGE}.po")
+    set(COMPEND_PO_FILE "${PROJ_L10N_VERSION_COMPEND_DIR}/${_LANGUAGE}.po")
     set(LOCALE_PO_DIR   "${PROJ_L10N_VERSION_LOCALE_DIR}/${_LANGUAGE}")
     set(LOCALE_POT_DIR  "${PROJ_L10N_VERSION_LOCALE_DIR}/pot")
     remove_cmake_message_indent()
@@ -38,7 +38,7 @@ foreach(_LANGUAGE ${LANGUAGE_LIST})
     message("_LANGUAGE_CROWDIN    = ${_LANGUAGE_CROWDIN}")
     message("TMS_CONFIG_FILE_PATH = ${TMS_CONFIG_FILE_PATH}")
     message("TMSTMP_PO_DIR        = ${TMSTMP_PO_DIR}")
-    message("TMSTMP_PO_FILE       = ${TMSTMP_PO_FILE}")
+    message("COMPEND_PO_FILE      = ${COMPEND_PO_FILE}")
     message("LOCALE_PO_DIR        = ${LOCALE_PO_DIR}")
     message("LOCALE_POT_DIR       = ${LOCALE_POT_DIR}")
     message("")
@@ -70,7 +70,7 @@ foreach(_LANGUAGE ${LANGUAGE_LIST})
     concat_po_from_locale_to_compendium(
         IN_WRAP_WIDTH        "${GETTEXT_WRAP_WIDTH}"
         IN_LOCALE_PO_DIR     "${TMSTMP_PO_DIR}"
-        IN_COMPEND_PO_FILE   "${TMSTMP_PO_FILE}")
+        IN_COMPEND_PO_FILE   "${COMPEND_PO_FILE}")
     message("")
     restore_cmake_message_indent()
 
@@ -81,7 +81,7 @@ foreach(_LANGUAGE ${LANGUAGE_LIST})
     merge_po_from_src_to_dst_with_compendium(
         IN_LANGUAGE               "${_LANGUAGE}"
         IN_WRAP_WIDTH             "${GETTEXT_WRAP_WIDTH}"
-        IN_SRC_COMPEND_PO_FILE    "${TMSTMP_PO_FILE}"
+        IN_SRC_COMPEND_PO_FILE    "${COMPEND_PO_FILE}"
         IN_SRC_LOCALE_PO_DIR      "${TMSTMP_PO_DIR}"
         IN_DST_LOCALE_PO_DIR      "${LOCALE_PO_DIR}"
         IN_DST_LOCALE_POT_DIR     "${LOCALE_POT_DIR}")
